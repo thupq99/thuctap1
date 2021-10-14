37b29d22f0ff506e76aaf4931e9c561e2cc558bb
QuangThuP9 <phamquangthu2@gmail.com.com>
2021-10-14 09:51:34 +0700

    first commit

---
+++ da-app/src/main/java/com/globits/DAApplication.java
@@ -0,0 +1,13 @@
+package com.globits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@SpringBootApplication
+//@EnableSwagger2
+public class DAApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DAApplication.class, args);
+	}
+}
